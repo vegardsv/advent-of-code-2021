cba7f8941e13296811b8b22883d8955806370bd5
Vegard Svendsen <vegardsvendsen@Vegards-MacBook-Pro.local>
2021-12-01 18:49:20 +0100

    day 1 refactor

--- 1/index.js
+++ 1/index.js
@@ -1,12 +1,9 @@
 const fs = require('fs');
 const xs = fs.readFileSync('input.txt').toString().split("\n").map(x => parseInt(x))
 
-let inc = xs => xs.reduce((a, c, i) => xs[i] > xs[i-1] ? a + 1 : a, 0);
+const inc = xs => xs.reduce((a, c, i) => xs[i] > xs[i-1] ? a + 1 : a, 0);
 
-let grouped = xs => xs.map((x, i) => x + xs[i-1] + xs[i+1])
+const grouped = xs => xs.map((x, i) => x + xs[i-1] + xs[i+1])
 
-// Part 1
-console.log("Answer part 1: " + inc(xs))
+console.log("Answer part 1: " + inc(xs), ",Answer part 2: " + inc(grouped(xs)) )
 
-// Part 2
-console.log("Answer part 2: " + inc(grouped(xs)))
